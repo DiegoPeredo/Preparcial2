1e178948e87fb1f4dd553c58d08b3b5d9fb00790
Diego Peredo <a2019118858@estudiantes.upsa.edu.bo>
2021-05-08 07:55:29 -0400

    Se adicionó el controlador People con token

---
+++ PrepParcial2PeredoDiego/PrepParcial2PeredoDiego/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PrepParcial2PeredoDiego.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<PrepParcial2PeredoDiego.Models.Person> People { get; set; }
+    }
+}
---
+++ PrepParcial2PeredoDiego/PrepParcial2PeredoDiego/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PrepParcial2PeredoDiego.Models
+{
+    public class Person
+    {
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength =30)]
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        [DataType(DataType.Date)]
+        public int BirthDay { get; set; }
+    }
+}
